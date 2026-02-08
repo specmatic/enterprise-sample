4e0be6ca022f013087902db6f0f94c10a1ccf5db
specmatic-builder <github-service-account@specmatic.io>
2026-02-08 21:47:57 +0530

    chore(deps): update plugin com.google.protobuf to v0.9.6 (#4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     kotlin("plugin.spring") version "2.3.0" apply false
     id("org.springframework.boot") version "3.2.6" apply false
     id("io.spring.dependency-management") version "1.1.5" apply false
-    id("com.google.protobuf") version "0.9.4" apply false
+    id("com.google.protobuf") version "0.9.6" apply false
 }
 
 group = "com.example"
