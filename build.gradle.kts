82e731eeef197c05371593bc85fb2bf41ed6a612
Specmatic Renovate <github-service-account@specmatic.io>
2026-02-08 04:47:58 +0000

    Update kotlin monorepo to v1.9.25

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.22" apply false
-    kotlin("plugin.spring") version "1.9.22" apply false
+    kotlin("jvm") version "1.9.25" apply false
+    kotlin("plugin.spring") version "1.9.25" apply false
     id("org.springframework.boot") version "3.2.6" apply false
     id("io.spring.dependency-management") version "1.1.5" apply false
     id("com.google.protobuf") version "0.9.4" apply false
