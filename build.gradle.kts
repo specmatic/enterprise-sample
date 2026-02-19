0656db2efc84a33a51846cb90ccae0ec3f7ae41c
Specmatic Renovate <github-service-account@specmatic.io>
2026-02-19 16:22:07 +0000

    chore(deps): update plugin org.springframework.boot to v4

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     kotlin("jvm") version "2.3.10" apply false
     kotlin("plugin.spring") version "2.3.10" apply false
-    id("org.springframework.boot") version "3.5.11" apply false
+    id("org.springframework.boot") version "4.0.3" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
     id("com.google.protobuf") version "0.9.6" apply false
 }
