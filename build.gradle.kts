2e44c34fef67c20a3d517dff0046bfc621ebf966
specmatic-builder <github-service-account@specmatic.io>
2026-03-18 06:03:52 +0530

    chore(deps): update kotlin monorepo to v2.3.20 (#24)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    kotlin("jvm") version "2.3.10" apply false
-    kotlin("plugin.spring") version "2.3.10" apply false
+    kotlin("jvm") version "2.3.20" apply false
+    kotlin("plugin.spring") version "2.3.20" apply false
     id("org.springframework.boot") version "3.5.11" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
     id("com.google.protobuf") version "0.9.6" apply false
